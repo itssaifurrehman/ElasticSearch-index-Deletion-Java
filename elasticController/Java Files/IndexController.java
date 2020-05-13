7943d3991345454dde1aad4bd26011df1bf2affb
Saif Ur Rehman <saif.rehman@telintel.net>
2020-05-13 13:10:48 -0500

    update

--- elasticController/Java Files/IndexController.java
+++ elasticController/Java Files/IndexController.java
@@ -1,43 +1,27 @@
 package com.elasticcontroller;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import com.google.common.base.Strings;
-
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.Node;
-import org.elasticsearch.client.Request;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.Response;
-import org.elasticsearch.client.ResponseException;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
-import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.http.Header;
 import org.apache.http.HttpHost;
-import org.apache.http.RequestLine;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 
 @Service
 public class IndexController {
@@ -46,10 +30,7 @@ public class IndexController {
 	private String appName;
 
 	@Value("${elastic.node1IPADDRESS}")
-	private String ipAddressNodeOne;
-
-	@Value("${elastic.node2IPADDRESS}")
-	private String ipAddressNodeTwo;
+	private String[] ipAddress;
 
 	@Value("${elastic.portNumber}")
 	private int portNumber;
@@ -69,8 +50,10 @@ public class IndexController {
 	@Value("${elastic.indicesRequestType}")
 	private String indicesPatternType;
 
-	private String[] lines = { "GARBAGE VALUE" };
+	private static RestHighLevelClient restHighLevelClient;
+	private static RestClient restLowLevelClient;
 
+	private String[] lines = { "GARBAGE VALUE" };
 	private String indexName = "NO_FILE_PRESENT";
 	private String defaultDifferenceIndexName = "NO_FILE_PRESENT";
 	private String defaultSameDayIndexName = "NO_FILE_PRESENT";
@@ -86,9 +69,7 @@ public class IndexController {
 
 	private Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-	// @Scheduled(fixedDelay = 5000)
-
-	@Scheduled(cron = "0 0 12 * * ?	")
+	@Scheduled(cron = "0 0 12 * * ? ")
 	public void indexDeletion() throws IOException {
 
 		Date difference = new Date(System.currentTimeMillis() - (86400000 * xOldDaysLogs));
@@ -100,30 +81,31 @@ public class IndexController {
 
 		try {
 			if (!setTempPrint) {
-				logger.info("[ " + appName + " ] application has been started with Elasticsearch's IP: ["
-						+ ipAddressNodeOne + ", and " + ipAddressNodeTwo + " ] with  Port: [ " + portNumber
-						+ " ] with Application Port: [ " + applicationPort
-						+ " ] and  Time limit of deleting the logs after every ["
-						+ (numberofDaysFormat.format(difference)) + "] days.");
+				logger.info("[ " + appName + " ] application has been started with Elasticsearch's IP: "
+						+ Arrays.toString(ipAddress) + " with  Port: [ " + portNumber + " ] with Application Port: [ "
+						+ applicationPort + " ] and  Time limit of deleting the logs after every [" + (xOldDaysLogs)
+						+ "] days.");
 				setTempPrint = true;
 			}
 
-			RestHighLevelClient client = new RestHighLevelClient(
-					RestClient.builder(new HttpHost(ipAddressNodeOne, portNumber, "http"),
-							new HttpHost(ipAddressNodeTwo, portNumber, "http")));
+			for (int i = 0; i < ipAddress.length; i++) {
+				restHighLevelClient = new RestHighLevelClient(
+						RestClient.builder(new HttpHost(ipAddress[i], portNumber, "http")));
+			}
 
-			RestClient restClient = RestClient
-					.builder(new HttpHost(ipAddressNodeOne, 9200, "http"), new HttpHost(ipAddressNodeTwo, 9200, "http"))
-					.build();
+			for (int i = 0; i < ipAddress.length; i++) {
+				restLowLevelClient = RestClient.builder(new HttpHost(ipAddress[i], portNumber, "http")).build();
+			}
+
 			Request request = new Request(indicesPatternType, indicesPattern);
 			try {
-				Response response = restClient.performRequest(request);
+				Response response = restLowLevelClient.performRequest(request);
 				responseBody = EntityUtils.toString(response.getEntity());
 				lines = responseBody.split("\r\n|\r|\n");
 				logger.info("[Indexes Names] Indexes present in the elasticsearch with prefix go4logs- are:\n"
 						+ responseBody);
 			} catch (Exception e) {
-				logger.warn("[Response Error] No Index File is found to delete.");
+				logger.warn("[Connnection Error]  Application can not make any connection with Elasticsearch");
 			}
 			if (lines.length > 1)
 				logger.info("[Files Count] Total files present: [" + lines.length + "]");
@@ -139,8 +121,8 @@ public class IndexController {
 							} else {
 								logger.info("[File Search] Looking for Index File: " + w + " to delete");
 								DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(w);
-								AcknowledgedResponse deleteIndexResponse = client.indices().delete(deleteIndexRequest,
-										RequestOptions.DEFAULT);
+								AcknowledgedResponse deleteIndexResponse = restHighLevelClient.indices()
+										.delete(deleteIndexRequest, RequestOptions.DEFAULT);
 								acknowledged = deleteIndexResponse.isAcknowledged();
 								if (acknowledged) {
 									logger.info("[DELETE Request] Index File: [" + w + "] is deleted successfully.");
@@ -153,8 +135,8 @@ public class IndexController {
 					logger.warn("[FILE NOT FOUND] No Index File is found.");
 				}
 			}
-			restClient.close();
-			client.close();
+			restLowLevelClient.close();
+			restHighLevelClient.close();
 		} catch (ResponseException e) {
 			logger.warn("[FILE NOT FOUND]No Indexed File is present with name [" + indexName + "] to delete.");
 		}
